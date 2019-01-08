539840b5f1bc7e256adb40155018b9d1c18da1ff
eselbeus <eselbeus@gmail.com>
2019-01-08 23:23:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     return array.join(" and ")
   else
-    array.slice()
+    new_array = array.slice(0, -1)
+    return new_array.join(", ") + " and " + array(-1)
   end
 end
