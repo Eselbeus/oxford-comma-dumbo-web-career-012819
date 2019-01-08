8ddda56f1de4f80719bbd200a9abd1f61fc23ef1
eselbeus <eselbeus@gmail.com>
2019-01-08 23:30:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  else
+    new_array = array.slice(0, array.size - 1)
+    return new_array.join(", ") + ", and " + array[-1]
+  end
 end
