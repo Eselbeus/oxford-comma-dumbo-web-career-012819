163edea3bfccde50687efcadcc1c5e36501740d0
eselbeus <eselbeus@gmail.com>
2019-01-08 23:30:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-require "pry"
+
 
 def oxford_comma(array)
   if array.size == 1
@@ -6,8 +6,8 @@ def oxford_comma(array)
   elsif array.size == 2 
     return array.join(" and ")
   else
-    new_array = array.slice(0, -1)
-    binding.pry
+    new_array = array.slice(0, array.size - 1)
+    
     return new_array.join(", ") + " and " + array[-1]
   end
 end
